f620d7e8ea3e71f538eb2e5abdf759734c880395
RaphaLourenX <83429579+RaphaLourenX@users.noreply.github.com>
2021-09-10 20:50:09 -0300

    Criação inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Main {
+	public static int SCREENRES_X = 600;
+	public static int SCREENRES_Y = 600;
+
+	public static void main(String args[]) {
+		new Screen(SCREENRES_X, SCREENRES_Y);
+	}
+}
---
+++ src/Particle.java
@@ -0,0 +1,9 @@
+
+public class Particle {
+	int x; int y;
+	
+	public Particle(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
